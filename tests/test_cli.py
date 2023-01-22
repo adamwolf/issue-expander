6ffa5ed284e4849d1a28e93da888f574ad68077e
Adam Wolf <adamwolf@feelslikeburning.com>
2023-01-21 19:45:02 -0600

    Refactor substitution and fix bugs
    I added tests that improve test coverage.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,9 @@ from click.testing import CliRunner
 
 from issue_expander.issue_expander import cli
 
+# TODO test username implies token
+# TODO test token implies username
+
 
 @responses.activate
 def test_file():
@@ -45,6 +48,18 @@ def test_stdin():
     assert result.output == "[Foobar the Frobnitz #101](https://example.com/pulls/106970)"
     assert result.exit_code == 0
 
+@responses.activate
+def test_404s_dont_expand():
+    """If something looks like an issue reference, but GitHub 404s, don't expand it."""
+    responses.get(
+        "https://api.github.com/repos/adamwolf/geewhiz/issues/101",
+        status = 404
+    )
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["-"], input="adamwolf/geewhiz#101")
+    assert result.output == "adamwolf/geewhiz#101"
+    assert result.exit_code == 0
 
 @responses.activate
 def test_default_source():
@@ -84,20 +99,23 @@ def test_default_source_but_not_needed():
     assert result.exit_code == 0
 
 
-# TODO test username implies token
-# TODO test token implies username
+@responses.activate
+def test_two_expansions_in_one_line():
+    """Test that we can expand multiple issues in one line"""
+    responses.get(
+        "https://api.github.com/repos/adamwolf/geewhiz/issues/101",
+        json={"html_url": "https://example.com/pulls/101", "title": "Foobar the Frobnitz"},
+    )
+    responses.get(
+        "https://api.github.com/repos/adamwolf/geewhiz/issues/102",
+        json={"html_url": "https://example.com/pulls/102", "title": "HOTFIX: fix the frobnitz!"},
+    )
 
-# @responses.activate
-# def test_two_expansions_in_one_line(): # TODO FIXME
-#     """Test that we can expand multiple issues in one line"""
-#     responses.get("https://api.github.com/repos/adamwolf/geewhiz/issues/101",
-#     json={"html_url": "https://example.com/pulls/101", "title": "Foobar the Frobnitz"})
-#     responses.get("https://api.github.com/repos/adamwolf/geewhiz/issues/102",
-#     json={"html_url": "https://example.com/pulls/102", "title": "HOTFIX: fix the frobnitz!"})
-#
-#     runner = CliRunner()
-#     result = runner.invoke(cli, ['-'], input='adamwolf/geewhiz#101 adamwolf/geewhiz#102')
-#     assert False
+    runner = CliRunner()
+    result = runner.invoke(cli, ["-"], input="adamwolf/geewhiz#101 adamwolf/geewhiz#102")
+    assert result.output == (
+        "[Foobar the Frobnitz #101](https://example.com/pulls/101) [HOTFIX: fix the frobnitz! #102](https://example.com/pulls/102)"
+    )
 
 
 @responses.activate
